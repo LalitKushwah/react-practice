d5279a26dedc39f892d4629c5b2d070a3a88bb13
LalitKushwah <lalitkushwah52525@gmail.com>
2021-11-29 08:08:33 +0530

    made minute functional

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { ADD_ITEM } from "./actions";
-import { useLocalStorage } from "./useLocalStorage";
+
 export const App = () => {
-  const [value, setValue] = useLocalStorage("name", "");
+  const [secDeg, setSecDeg] = useState();
+  const [minDeg, setMinDeg] = useState();
+  const [hourDeg, setHourDeg] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const time = new Date();
+      const second = time.getSeconds();
+      const minute = time.getMinutes();
+      const hour = time.getHours();
+      console.log("==== hour ===", hour);
+      const secondDeg = (second / 60) * 360 + 90;
+      const minuteDeg = (minute / 60) * 360 + 90;
+      const hourDeg = (hour / 12) * 360 + 90;
+      setSecDeg(secondDeg);
+      setMinDeg(minuteDeg);
+      setHourDeg(hourDeg);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [secDeg, minDeg]);
 
   return (
-    <div className="App">
-      <div className="control">
-        <input value={value} onChange={(e) => setValue(e.target.value)} />
+    <div className="clock">
+      <div className="clock-face">
+        <div
+          className="hand second-hand"
+          style={{ transform: `rotate(${secDeg}deg)` }}
+        ></div>
+        <div
+          className="hand minute-hand"
+          style={{ transform: `rotate(${minDeg}deg)` }}
+        ></div>
+        <div
+          className="hand hour-hand"
+          style={{ transform: `rotate(${hourDeg}deg)` }}
+        ></div>
       </div>
     </div>
   );
