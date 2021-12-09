6c78e72669eb802f6bc6dbac76f67d721f7c0bf6
LalitKushwah <lalitkushwah52525@gmail.com>
2021-12-09 21:55:58 +0530

    react query

--- src/App.js
+++ src/App.js
@@ -4,16 +4,46 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { ADD_ITEM } from "./actions";
-import { useLocalStorage } from "./useLocalStorage";
-export const App = () => {
-  const [value, setValue] = useLocalStorage("name", "");
+export const App = ({ articles, addArticle }) => {
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextSecond = second + 1;
+      if (nextSecond > 60) {
+        setSecond(0);
+        // need to handle scenario when minute = 60;
+        setMinute((minute) => minute + 1);
+      } else {
+        setSecond((second) => second + 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <div className="App">
-      <div className="control">
-        <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <div>
+        <h1>
+          {minute}:{second}
+        </h1>
       </div>
+      <div>
+        <button onClick={() => addArticle({ title: "Test Item", price: 100 })}>
+          Add Item
+        </button>
+      </div>
+      <h2>{JSON.stringify(articles)}</h2>
     </div>
   );
 };
-export default App;
+
+const mapStateToProps = (state) => ({
+  articles: state.articles,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addArticle: (data) => dispatch({ type: "ADD_ITEM", payload: data }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/root.reducer.js
+++ src/root.reducer.js
@@ -13,3 +13,6 @@ export const rootReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+
+
